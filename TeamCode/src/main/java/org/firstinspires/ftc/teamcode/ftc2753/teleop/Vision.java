4611d28f30ff18c74d1a547e98b1cd9e33aeb336
Timothy <tmeehan26@gmail.com>
2019-11-17 20:47:15 -0500

    4.11.17.19d

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ftc2753/teleop/Vision.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ftc2753/teleop/Vision.java
@@ -4,6 +4,7 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.firstinspires.ftc.robotcore.external.stream.CameraStreamSource;
 import org.firstinspires.ftc.teamcode.ftc2753.NotSkystoneDetector;
 import org.openftc.easyopencv.OpenCvCamera;
 import org.openftc.easyopencv.OpenCvCameraRotation;
